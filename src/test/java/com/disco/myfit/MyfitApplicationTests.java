3da58651d374d4b6df25680fd02d0d8408b1f4cb
jaeyunkim <rlawodbs1024@gmail.com>
2024-03-13 22:07:01 +0900

    Initial commit

---
+++ src/test/java/com/disco/myfit/MyfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disco.myfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
